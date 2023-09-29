7768dce49f02731d385fa9611e55a9f3dd1a453a
Monica Martin Rodriguez <132091193+proyectosprogramacion@users.noreply.github.com>
2023-09-29 06:37:34 +0200

    incorporado libreria thymeleaf

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -9,13 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoApplication {
 
-	public static void main(String[] args) { 
-		SpringApplication.run(DemoApplication .class, args); 
-		}
-		 	@GetMapping("/hello") 
-		public String hello() { 
-		return String.format("Hello World !!");
-		 }
-
-
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
 }
